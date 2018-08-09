38052a1419f6c29a219d8d87202bc4941f7fe681
wpbdry <wpbdry@gmail.com>
2018-08-09 14:38:45 +0200

    fixed file extention

---
+++ Lesson 2/Quiz 2.py
@@ -0,0 +1,5 @@
+# Write Python code that prints out the number of hours in 7 weeks.
+
+weeks = 7 # change this to calculate hours for a different  number of weeks
+
+print ('There are ' + str(weeks * 7 * 24) + ' hours in ' + str(weeks) + ' weeks')
